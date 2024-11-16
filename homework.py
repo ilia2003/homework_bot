f38e95103d8fe58c4a3286867ea4ad2ca5566d38
Fedorenko Ilia <fedorenkoilia@gmail.com>
2024-11-16 17:19:11 +0100

    1

--- homework.py
+++ homework.py
@@ -69,7 +69,9 @@ def get_api_answer(timestamp):
         response = requests.get(ENDPOINT, headers=HEADERS, params=params)
         if response.status_code != 200:
             logger.error(f"Ошибка: код ответа API - {response.status_code}")
-            raise ValueError(f"Ошибка API: код ответа - {response.status_code}")
+            raise ValueError(
+                f"Ошибка API: код ответа - {response.status_code}"
+            )
         return response.json()
     except requests.exceptions.RequestException as error:
         logger.error(f"Ошибка при запросе к API: {error}")
